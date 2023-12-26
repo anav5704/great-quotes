c099204be26851170f9718c0e36698d3318dc350
anav5704 <anavkki@gmail.com>
2023-12-26 20:55:05 +1200

    feat(api): add get all quotes

--- src/server/api/routers/quote.ts
+++ src/server/api/routers/quote.ts
@@ -1,16 +1,17 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { currentUser } from "~/app/lib/currentUser"
 import { z } from "zod";
+import { db } from "~/server/db";
 
 export const quoteRouter = createTRPCRouter({
     createQuote: publicProcedure
-    .input(z.object({
-        content: z.string(),
-    }))
-    .mutation(async ({ ctx, input }) => {
+        .input(z.object({
+            content: z.string(),
+        }))
+        .mutation(async ({ ctx, input }) => {
             const user = await currentUser()
             const { content } = input
-            if(!user) return
+            if (!user) return
 
             const quote = await ctx.db.quote.create({
                 data: {
@@ -20,5 +21,11 @@ export const quoteRouter = createTRPCRouter({
             })
 
             return quote
+        }),
+
+    getQuotes: publicProcedure
+        .query(async ({ ctx }) => {
+            const quotes = await db.quote.findMany()
+            return quotes
         })
 });
