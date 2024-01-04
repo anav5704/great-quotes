ca7a00170c461c9612c8fa158ff305f9cd6351f9
anav5704 <anavkki@gmail.com>
2024-01-04 14:15:18 +1200

    refactor(api): split likes router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,8 +1,10 @@
 import { quoteRouter } from "~/server/api/routers/quote";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { likeRouter } from "./routers/like";
 
 export const appRouter = createTRPCRouter({
   quote: quoteRouter,
+  like: likeRouter
 });
 
 export type AppRouter = typeof appRouter;
--- src/server/api/routers/quote.ts
+++ src/server/api/routers/quote.ts
@@ -88,41 +88,6 @@ export const quoteRouter = createTRPCRouter({
             return quotes
         }),
 
-    getLikedQuotes: publicProcedure
-        .input(z.object({
-            userId: z.string(),
-            page: z.number()
-        }))
-        .query(async ({ ctx, input }) => {
-            const { userId, page } = input
-
-            const liked = await ctx.db.like.findMany({
-                where: {
-                    userId
-                },
-                include: {
-                    quote: {
-                        include: {
-                            user: true,
-                            likes: true
-                        }
-                    }
-                },
-                orderBy: {
-                    quote: {
-                        createdAt: "desc"
-                    }
-                },
-                skip: 6 * page,
-                take: 6
-            })
-
-            const quotes = liked.map((like) => like.quote)
-
-            // return decrypt(quotes)
-            return quotes
-        }),
-
     updateQuote: publicProcedure
         .input(z.object({
             content: z.string().min(1, { message: "Bro at least type the quote out 🤦" }).max(100, { message: "That's not a quote, that's an essay 💀" }),
@@ -163,39 +128,4 @@ export const quoteRouter = createTRPCRouter({
 
             return quote
         }),
-
-    toggleLike: publicProcedure
-        .input(z.object({
-            quoteId: z.string(),
-        }))
-        .mutation(async ({ ctx, input }) => {
-            const user = await currentUser()
-            const { quoteId } = input
-            if (!user) return
-
-            const isLiked = await ctx.db.like.findFirst({
-                where: {
-                    userId: user.id,
-                    quoteId,
-                }
-            })
-
-            if (isLiked) {
-                await ctx.db.like.deleteMany({
-                    where: {
-                        userId: user.id,
-                        quoteId
-                    }
-                })
-            }
-
-            else {
-                await ctx.db.like.create({
-                    data: {
-                        userId: user.id,
-                        quoteId
-                    }
-                })
-            }
-        })
 });
