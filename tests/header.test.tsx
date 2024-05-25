2246891ce99de84e69d0f4d0405b1cf9cd9d266c
Anav Chand <anavkki@gmail.com>
2024-05-25 20:55:01 +1200

    test: add test for header

---
+++ tests/header.test.tsx
@@ -0,0 +1,22 @@
+import "@testing-library/jest-dom"
+import { render, screen } from "@testing-library/react"
+
+import { Header } from "@/components/header"
+
+describe("Header", () => {
+    it("Should render h1 with text", () => {
+        render(<Header>Hello World</Header>)
+
+        const heading = screen.getByRole("heading", { level: 1 })
+
+        expect(heading).toBeInTheDocument()
+    })
+
+    it("Should have correct text", () => {
+        render(<Header>Hello World</Header>)
+
+        const heading = screen.getByRole("heading", { level: 1 })
+
+        expect(heading).toHaveTextContent("Hello World")
+    })
+})
