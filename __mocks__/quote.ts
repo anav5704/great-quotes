8642629e123f5ee4494ce98330e5f7f65d705399
Anav Chand <anavkki@gmail.com>
2024-06-22 20:15:17 +1200

    test: add mock data for tests

---
+++ __mocks__/quote.ts
@@ -0,0 +1,10 @@
+import { user } from "./user";
+
+export const quote = {
+    id: '123',                                                    
+    content: "There's no place like home",                             
+    userId: '456',                                                
+    createdAt: '2024-06-18 20:18:47.279',                                                          
+    updatedAt: '2024-06-18 20:18:47.279',                                                          
+    User: user                                                                                         
+}  
