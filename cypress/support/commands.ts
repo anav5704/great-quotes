3b0a2759260960eb7cbc00706bdaa80af9e98c91
Anav Chand <anavkki@gmail.com>
2024-06-25 17:35:38 +1200

    test: cypress next auth login

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,30 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+Cypress.Commands.add('login', () => {
+    cy.request('GET', 'http://localhost:3000/api/auth/csrf').then((csrfResp) => {
+        const csrfToken = csrfResp.body.csrfToken
+
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3000/api/auth/callback/credentials',
+            body: {
+                email: Cypress.env("GOOGLE_EMAIL"),
+                password: Cypress.env("GOOGLE_PASSWD"),
+                csrfToken: csrfToken
+            },
+            followRedirect: false
+        }).then((loginResp) => {
+            if (loginResp.status === 302 && loginResp.redirectedToUrl) {
+                cy.visit(loginResp.redirectedToUrl).then(() => {
+                    cy.getCookies().then((cookies) => {
+                        cookies.forEach((cookie) => {
+                            cy.setCookie(cookie.name, cookie.value)
+                        })
+                    })
+                })
+            } else {
+                throw new Error('Unexpected response during login')
+            }
+        })
+    })
+})
